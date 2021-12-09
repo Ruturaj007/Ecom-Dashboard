0ed4aa57a6d3fd9c8c943394cc33b89e269f8ef3
Ruturaj007 <joshirutu@ymail.com>
2021-12-09 09:37:34 -0800

    Added condition in Users.js

--- src/Components/TO DO List/User.js
+++ src/Components/TO DO List/User.js
@@ -4,9 +4,11 @@ import { withRouter } from "react-router";
 const User = (props) => {
   return (
     <div>
-      <h1> Welcome <Badge bg="primary">{props.match.params.name}</Badge></h1> 
-      <h1> Your id is {props.match.params.id}</h1>
-    </div>
+        {props.match.params.name ==null ? alert("No data to show") :<div><h1> Welcome <Badge bg="primary">{props.match.params.name}</Badge></h1> 
+        <h1> Your id is {props.match.params.id}</h1></div> }
+      </div>
+        
+      
   );
 };
 export default withRouter(User);
