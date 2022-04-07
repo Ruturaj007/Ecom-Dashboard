a251c0772d42c4d81251a084036405ef7adbd3a0
Ruturaj007 <joshirutu@ymail.com>
2022-04-07 09:21:09 -0700

    [UPDATE]-Drag_Drop.js

--- src/Components/Drag_And_Drop/Drag_Drop.js
+++ src/Components/Drag_And_Drop/Drag_Drop.js
@@ -3,21 +3,29 @@ import List from "./Data.json";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 const DDAR = () => {
   const [list, setList] = useState(List);
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+  };
   const myFunction = (result) => {
     console.log(result);
+    setList(reorder());
   };
+
   // Droppable Gives set of rander props.
   // provided, snapshot is bunch of data that lets you bind all properties of Droppable with div inside it
   // ref={provided.innerRef()} will bind Droppable to div.
   // Draggable items to drag . It has id which is unique , key and index from data
   //  {...provided.draggableProps} gives all the data so drag can happen
-  // DragHandleProps says that the div which it is wrapped around with is your drag handle . 
+  // DragHandleProps says that the div which it is wrapped around with is your drag handle .
   // So basically you can just grab the whole div and drag and drop
   return (
     <DragDropContext onDragEnd={myFunction}>
       <Droppable droppableId="AnyString">
         {(provided, snapshot) => {
-          <div class="Apple" ref={provided.innerRef}>
+          <div ref={provided.innerRef}>
             <h1 style={{ paddingLeft: "650px" }}> Data From JSON </h1>
             <table style={{ fontSize: "30px", Padding: "30px" }}>
               <tbody>
@@ -49,6 +57,7 @@ const DDAR = () => {
                 ))}
               </tbody>
             </table>
+            {provided.placeholder}
           </div>;
         }}
       </Droppable>
